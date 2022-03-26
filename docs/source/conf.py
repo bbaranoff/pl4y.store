90b467c754832c6a64cb5b93a4009115d64134d3
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-03-26 15:19:56 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RF-eXploring'
+copyright = '2022, Baranoff'
+author = 'Bastien Baranoff'
 
 release = '0.1'
 version = '0.1.0'
